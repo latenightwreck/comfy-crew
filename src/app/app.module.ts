6d8e23cdd98eb43df4b1e5e4245d256cc96c4d35
latenightwreck <morgan.b.armstrong@gmail.com>
2018-10-08 14:38:00 -0400

    Add new modules to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { XivapiClientModule } from '@xivapi/angular-client';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    XivapiClientModule.forRoot('cd1e255e760b445799d4a581')
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
