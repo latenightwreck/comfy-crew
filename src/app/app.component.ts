8ecd9ab4cc6d893c976fc75bbc88b9b2195c5c09
latenightwreck <morgan.b.armstrong@gmail.com>
2018-10-08 14:36:04 -0400

    Changed prefix to comfy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'comfy-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
