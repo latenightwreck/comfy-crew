d4c2ac5d01e7ba973f8ee4a46e72b4612aa72dc9
latenightwreck <morgan.b.armstrong@gmail.com>
2018-10-08 17:20:30 -0400

    Addition of header for test looks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,23 @@ import { AppComponent } from './app.component';
 import { XivapiClientModule } from '@xivapi/angular-client';
 import { HeaderComponent } from './header/header.component';
 
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    XivapiClientModule.forRoot('cd1e255e760b445799d4a581')
+    MatToolbarModule,
+    XivapiClientModule.forRoot('cd1e255e760b445799d4a581'),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
