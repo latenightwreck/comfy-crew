db69b7e5be6b9e6333e74c244260e3f43cb77eea
Morgan Blaze <morgan.b.armstrong@gmail.com>
2018-10-13 11:11:41 -0400

    access MatSidenav compnent from app component in header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'comfy-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent  {
+export class HeaderComponent {
+  @Input() sideNav: MatSidenav;
 
 }
