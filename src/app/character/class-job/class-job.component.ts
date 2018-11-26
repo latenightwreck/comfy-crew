e5f636577ac3d09e5b1e904fa0110725bbf4d27b
Morgan Blaze <morgan.b.armstrong@gmail.com>
2018-11-25 21:34:24 -0500

    Addition of class-job component

---
+++ src/app/character/class-job/class-job.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'comfy-class-job',
+  templateUrl: './class-job.component.html',
+  styleUrls: ['./class-job.component.css']
+})
+export class ClassJobComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
