6baf03518e4aef8a06226b713c861d48caa79de2
Morgan Blaze <morganblaze@Morgans-MacBook-Pro.local>
2018-12-01 23:58:59 -0500

    Addd server dropdown, and get name into a form

--- src/app/search/character-search/character-search/character-search.component.ts
+++ src/app/search/character-search/character-search/character-search.component.ts
@@ -6,10 +6,89 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./character-search.component.scss']
 })
 export class CharacterSearchComponent implements OnInit {
+  objectKeys = Object.keys;
+  readonly SERVER_LIST: Object = {
+    Aether: [
+      'Adamantoise',
+      'Balmung',
+      'Cactuar',
+      'Coeurl',
+      'Faerie',
+      'Gilgamesh',
+      'Goblin',
+      'Jenova',
+      'Mateus',
+      'Midgardsormr',
+      'Sargatanas',
+      'Siren',
+      'Zalera'
+    ],
+    Chaos: [
+      'Cerberus',
+      'Lich',
+      'Louisoix',
+      'Moogle',
+      'Odin',
+      'Omega',
+      'Phoenix',
+      'Ragnarok',
+      'Shiva',
+      'Zodiark'
+    ],
+    Elemental: [
+      'Aegis',
+      'Atomos',
+      'Carbuncle',
+      'Garuda',
+      'Gungnir',
+      'Kujata',
+      'Ramuh',
+      'Tonberry',
+      'Typhon',
+      'Unicorn'
+    ],
+    Gaia: [
+      'Alexander',
+      'Bahamut',
+      'Durandal',
+      'Fenrir',
+      'Ifrit',
+      'Ridill',
+      'Tiamat',
+      'Ultima',
+      'Valefor',
+      'Yojimbo',
+      'Zeromus'
+    ],
+    Mana: [
+      'Anima',
+      'Asura',
+      'Belias',
+      'Chocobo',
+      'Hades',
+      'Ixion',
+      'Mandragora',
+      'Masamune',
+      'Pandaemonium',
+      'Shinryu',
+      'Titan'
+    ],
+    Primal: [
+      'Behemoth',
+      'Brynhildr',
+      'Diabolos',
+      'Excalibur',
+      'Exodus',
+      'Famfrit',
+      'Hyperion',
+      'Lamia',
+      'Leviathan',
+      'Malboro',
+      'Ultros'
+    ]
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
