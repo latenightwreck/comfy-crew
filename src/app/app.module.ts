3561ca57874f284bb5caef8745d9dc3eaa45eba4
Morgan Blaze <morganblaze@Morgans-MacBook-Pro.local>
2018-12-01 22:58:32 -0500

    Remove side-nav, start with character search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,21 @@ import { SearchBarComponent } from './search-bar/search-bar.component';
 import {FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing';
-import { SideNavComponent } from './side-nav/side-nav.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CharacterComponent } from './character/character.component';
 import { ClassJobComponent } from './character/class-job/class-job.component';
 import { ClassJobListComponent } from './character/class-job-list/class-job-list.component';
+import { CharacterSearchComponent } from './search/character-search/character-search/character-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SearchBarComponent,
-    SideNavComponent,
     CharacterComponent,
     ClassJobComponent,
-    ClassJobListComponent
+    ClassJobListComponent,
+    CharacterSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/search/character-search/character-search/character-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'comfy-character-search',
+  templateUrl: './character-search.component.html',
+  styleUrls: ['./character-search.component.scss']
+})
+export class CharacterSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
